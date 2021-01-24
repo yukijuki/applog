361bd8b6e381bbc2fa9df10e10f0b1334e714e98
Yuki Kawano <yukijukisfc@gmail.com>
2021-01-25 01:03:11 +0900

    home and upload html updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 import datetime, os, secrets
-from werkzeug.utils import secure_filename
-from PIL import Image
 import pyrebase
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import request, session, redirect, send_from_directory, jsonify, render_template, make_response, url_for, abort, flash
-from app import app, db
+from app import app, db, storage
 from app.function import session_verify
+from werkzeug.utils import secure_filename
+from PIL import Image
+import uuid
+
 
 @app.route("/")
 def index():
@@ -22,68 +26,65 @@ def project():
 
     return render_template("project.html")
 
-@app.route("/profile", methods=["GET", "POST"])
-def profile():
-    session_verify()
-    student = Student.query.filter_by(email=email).first()
+# @app.route("/profile", methods=["GET", "POST"])
+# def profile():
+#     session_verify()
+#     student = Student.query.filter_by(email=email).first()
 
-    if  request.method == "POST":
-        data = request.get_json()
+#     if  request.method == "POST":
+#         data = request.get_json()
 
-        if data["password"] == "":
-            data["password"] = student.password
+#         if data["password"] == "":
+#             data["password"] = student.password
 
-        if data["name"] == '':
-            data["name"] = student.name
+#         if data["name"] == '':
+#             data["name"] = student.name
 
-        if data["industry"] == '':
-            data["industry"] = student.industry
+#         if data["industry"] == '':
+#             data["industry"] = student.industry
 
-        student.password = data["password"]
-        student.name = data["name"]
-        student.industry = data["industry"]
-        db.session.commit()
-        flash("変更されました。")
+#         student.password = data["password"]
+#         student.name = data["name"]
+#         student.industry = data["industry"]
+#         db.session.commit()
+#         flash("変更されました。")
 
 
-        session["Industry"] = data["industry"]
+#         session["Industry"] = data["industry"]
 
-        response = make_response(jsonify(data, 200))
-        return response
+#         response = make_response(jsonify(data, 200))
+#         return response
 
-    return render_template("profile.html", data = student)
+#     return render_template("profile.html", data = student)
 
 @app.route("/screen", methods=["GET"])
 def screen():
+
+    # "screen_id": screen_id,
+    # "project_name": project_name,
+    # "screen_name": screen_name,
+    # "screen_category": screen_category,
+    # "log":
+
     session_verify()
     try:        
-        """
-        data = {
-            "faculty" = "Str",
-            "firm" = "Str",
-            "industry" = "Str",
-            "position": "Str",
-            "lab" = "Str",
-            "club" = "Str",
-        }
-        """
-        data = db.child("screens").get()
-        print(data.val())
-        screens = [
-            {
-                "id": "1",
-                "filename": "hey",
-                "name": "screen1"
+        data = db.child("screen").get()
+        render_all_screens = []
 
+        for screen in data.each():
+            print(screen.val())
+            render_screen = {
+                "screen_id": screen.val()["screen_id"],
+                "screen_name": screen.val()["screen_name"],
+                "screen_image_name": storage.child("image/"+screen.val()["screen_id"]).get_url(None)
             }
-        ]
-
+            render_all_screens.append(render_screen)
 
     except ConnectionError:
         abort(404)
         flash("なんかバグった")
 
-    return render_template("screen.html", screens = screens)
+    return render_template("screen.html", screens = render_all_screens)
 
 @app.route("/log/<id>", methods=["GET"])
 def log(id):
@@ -114,27 +115,41 @@ def log(id):
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
 
-    """
-    "screen_id": 1,
-    "project": "paypay",
-    "screen_name": "p2phome",
-    "screen_category": "p2p",
-    "screen_image_name": "screen_image1",
-    "correspondence": []
-    "log": []
-    """
-
     if request.method == "POST":
         print("chekc")
         if request.form:
-
             data = request.form
-            print(data)
-            # image = request.files["image"]
+            screen_id = str(uuid.uuid4())
+            project_name = "paypay"
+            screen_name = data["screen_name"]
+            screen_category = data["screen_category"]
+            image = request.files["screen_image_name"]
+            image.filename = screen_id
+            log = []
 
-            # if image.filename == "":
-            #     flash("Image must have a name")
-            #     return redirect(request.url)
+            screen = {
+                "screen_id": screen_id,
+                "project_name": project_name,
+                "screen_name": screen_name,
+                "screen_category": screen_category,
+                "log": [
+
+                    {
+                        "log_id":"log_id",
+                        "event_name": "event_name",
+                        "event_category": "event_category",
+                        "firebase_screen": "firebase_screen",
+                        "event_action": "event_action",
+                        "event_label": "event_label",
+                        "event_label2": "event_label2",
+                        "location_num": "location_num"
+                    }
+
+                ]
+            }
+
+            db.child("screen/"+screen_id).set(screen)
+            storage.child("image/"+screen_id).put(image)
             
             # if not allowed_image(image.filename):
             #     flash("PNG, JPG, JPEGを選んでください")
@@ -146,32 +161,9 @@ def upload():
             #         flash("ファイル名を変更してください")
             #         return redirect(request.url)
 
-            #     image.save(os.path.join(app.config["UPLOAD_FOLDER"], image.filename))
+            flash("新しいスクリーンが追加されました")
 
-            #     img = Image.open(os.path.join(app.config["UPLOAD_FOLDER"], image.filename))
-            #     img = crop_max_square(img)
-            #     img_resize_lanczos = img.resize((350, 350), Image.LANCZOS)
-            #     img_resize_lanczos.save(os.path.join(app.config["GET_FOLDER"], image.filename))
-            #     print(filename)
-
-            #     employee = Employee(
-            #     name = data["name"],
-            #     filename = filename,
-            #     link = data["link"],
-            #     faculty = data["faculty"],
-            #     firm = data["firm"],
-            #     industry = data["industry"],
-            #     position = data["position"],
-            #     lab = data["lab"],
-            #     club = data["club"],
-            #     ask_clicks = 0
-            #     )
-
-            #     db.session.add(employee)
-            #     db.session.commit()
-            #     flash("Image saved")
-
-            # return redirect(request.url)
+            # render_template("upload.html") with parameter here
     return render_template("upload.html")
 
 
